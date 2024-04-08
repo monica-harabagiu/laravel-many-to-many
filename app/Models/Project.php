a53f3469306f9bb4d8d8144ddf4a22cc5d2e1240
monica-harabagiu <monica.harabagiu.design@gmail.com>
2024-04-08 15:13:13 +0200

    technologies table migration and seeding and pivot table

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Support\Str;
 
 class Project extends Model
@@ -28,5 +29,10 @@ class Project extends Model
     public function type(): BelongsTo
     {
         return $this->belongsTo(Type::class);
+    }
+
+    public function technology(): BelongsToMany
+    {
+        return $this->belongsToMany(Technology::class);
     }
 }
